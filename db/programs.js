4b4e22d9a6e9fa0b77c02fb157e5b1bd13a17a68
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-30 16:51:31 -0500

    delete takes an integer argument

--- db/programs.js
+++ db/programs.js
@@ -4,16 +4,29 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("programs.sqlite");
 
 module.exports.new = () => {
+  return new Promise((resolve, reject) => {
+    resolve(1);
+  });
 };
 
-module.exports.delete = () => {
-
+module.exports.delete = id => {
+  return new Promise((resolve, reject) => {
+    if (id == undefined) {
+      let err = new Error("Please provide the ID of a training program to delete.");
+      reject(err);
+    }
+    resolve(1);
+  });
 };
 
 module.exports.getOne = id => {
-
+  return new Promise((resolve, reject) => {
+    resolve({});
+  });
 };
 
 module.exports.getAll = () => {
-
+  return new Promise((resolve, reject) => {
+    resolve([{}]);
+  });
 };
--- test/db.test.js
+++ test/db.test.js
@@ -8,20 +8,48 @@ describe("programs database interxns", () => {
     it("should be a function", () => {
       assert.isFunction(programs.new);
     });
+    it("should return a promise", () => {
+      assert.typeOf(programs.new(), "promise");
+    });
   });
   describe("delete()", () => {
     it("should be a function", () => {
       assert.isFunction(programs.delete);
+    });
+    it("should return a promise", () => {
+      assert.typeOf(programs.delete().catch(err => {}), "promise");
+    });
+    it("should expect an integer as argument", () => {
+      programs.delete()
+        .then(response => {
+          assert.isTrue(false);
+        })
+        .catch(err => {
+          assert.isTrue(true);
+        });
+      programs.delete(1)
+      .then(response => {
+        assert.isTrue(true);
+      })
+      .catch(err => {
+        assert.isTrue(false);
+      });
     });
   });
   describe("getAll()", () => {
     it("should be a function", () => {
       assert.isFunction(programs.getAll);
     });
+    it("should return a promise", () => {
+      assert.typeOf(programs.getAll(), "promise");
+    });
   });
   describe("getOne()", () => {
     it("should be a function", () => {
       assert.isFunction(programs.getOne);
+    });
+    it("should return a promise", () => {
+      assert.typeOf(programs.getOne(), "promise");
     });
   });
 });
