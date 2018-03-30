d5aefedac784a8161e149bf0dc5f87454a5f3999
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-30 16:38:31 -0500

    installed sqlite3, set up npm test and npm gen:db

---
+++ db.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("programs.sqlite");
+
+module.exports = () => {
+  db.serialize(() => {
+    db.run(`DROP TABLE IF EXISTS Programs`);
+    db.run(`CREATE TABLE IF NOT EXISTS Programs (
+      id INTEGER PRIMARY KEY,
+      seats INTEGER,
+      instructor TEXT,
+      start_date TEXT,
+      end_date TEXT,
+      category TEXT
+    )`);
+  });
+};
